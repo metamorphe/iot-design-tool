646909caa0781008df9ae23d93bdbd63bf43778b
Jasper O'Leary <jhaazpr@gmail.com>
2015-10-22 15:37:58 -0700

    Add basic energy ring functionality
    Click on the canvas to create a node, hold shift and drag
    to create concentric rings.

--- web-app/public/js/iot-design-tool.js
+++ web-app/public/js/iot-design-tool.js
@@ -1,20 +1,88 @@
-/* Load PaperScope */
+/* Constants and Globals */
+DEFAULT_COLOR = '#e9e9ff';
 
+var hitOptions = {
+    segments: true,
+    stroke: true,
+    fill: true,
+    tolerance: 5
+};
+
+var onKeyDown = function(event) {
+    switch (event.key) {
+        case 'a':
+            project.selectAll();
+            break;
+        case 'backspace':
+            event.preventDefault();
+            event.stopPropagation();
+            project.selectedItems.forEach(function(v) {
+                v.remove();
+            });
+            break;
+        case 'escape':
+            project.deselectAll();
+            break;
+        default:
+            break;
+    }
+}
+
+var recent;
+var recentCenter;
+
+/* Node Tool */
+var nodeTool = new paper.Tool();
+nodeTool.onMouseDown = function(event) {
+    recent = null;
+    project.activeLayer.selected = false;
+    var hitResult = project.hitTest(event.point, hitOptions);
+    if (true) { // was if (!hitResult)
+        var point = new Point(event.point);
+        var circle = new Path.Circle({
+            center: point,
+            radius: 20,
+            fillColor: DEFAULT_COLOR,
+        });
+        circle.selected = true;
+        recent = circle;
+        //$scope.nodeGroup.addChild(circle);
+    } else {
+        hitResult.item.selected = true;
+        recent = hitResult.item;
+        recentPath = new Path();
+        recentPath.strokeColor = '#000000';
+        recentPath.add(event.point);
+    }
+    recentCenter = event.point;
+}
+
+nodeTool.onMouseDrag = function(event) {
+    var hitResult = project.hitTest(event.point, hitOptions);
+    // Draw lines on shift
+    if (event.modifiers.shift && recentCenter) {
+        var circle = new Path.Circle({
+            center: recentCenter,
+            radius: recentCenter.getDistance(event.point),
+            fillColor: {alpha: 0.0 },
+            strokeColor: DEFAULT_COLOR,
+            strokeWidth: 1
+        });
+    }
+    else if (recent) {
+        recent.position = recent.position.add(event.delta);
+    }
+}
+nodeTool.minDistance = 20;
+nodeTool.onKeyDown = onKeyDown;
+
+/* Main */
 window.onload = function() {
     // Get a reference to the canvas object
     var canvas = document.getElementById('main-canvas');
     // Create an empty project and a view for the canvas:
     paper.setup(canvas);
-    // Create a Paper.js Path to draw a line into it:
-    var path = new paper.Path();
-    // Give the stroke a color
-    path.strokeColor = 'black';
-    var start = new paper.Point(100, 100);
-    // Move to start and draw a line from there
-    path.moveTo(start);
-    // Note that the plus operator on Point objects does not work
-    // in JavaScript. Instead, we need to call the add() function:
-    path.lineTo(start.add([ 200, -50 ]));
-    // Draw the view now:
+    paper.install(window);
+    nodeTool.activate();
     paper.view.draw();
 }
